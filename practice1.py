85e5a8b58e4de219076b112c9caad17a7f939030
mahamyousaf479-maker <mahamyousaf479@gmail.com>
2026-05-16 17:25:18 -0700

    practice string
    practice string

---
+++ practice1.py
@@ -0,0 +1,20 @@
+#practice ode
+LahoreInfo = "lahore is the capital of Punjab and Islamabad is the capital of Pakistan"
+print(LahoreInfo)
+print(type(LahoreInfo))
+
+print(LahoreInfo.upper())
+print(LahoreInfo.lower())
+print(LahoreInfo.swapcase())
+print(LahoreInfo.title())
+print(LahoreInfo.capitalize())
+
+print(LahoreInfo[0:6:1])
+print(LahoreInfo[25:31:1])
+print(LahoreInfo[36:45:1])
+
+print(len(LahoreInfo))
+
+for cha in LahoreInfo:
+    print(cha)
+    
