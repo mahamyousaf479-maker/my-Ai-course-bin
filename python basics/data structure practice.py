bb44293e930e72e285a30a6f5e3a7490f61d87b6
Maham Yousaf <mahamyousaf479@gmail.com>
2026-08-15 06:35:56 -0700

    Data structure

---
+++ python basics/data structure practice.py
@@ -0,0 +1,14 @@
+# my comments 
+Booklist = [23231 , "thick and grow rich" , "ABC Inc" , 34.35 , "William Richard" , True]
+print(Booklist)
+print(type(Booklist))
+print(len(Booklist))
+for x in Booklist:
+    print(x)
+
+Booklist.append("XYZ Inc")
+
+print(Booklist)
+
+Booklist.insert(0,"Robert")
+print(Booklist)
